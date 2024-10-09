c4441dff5dfc79f5b2cba50ace3d8413d514ac78
Sys-stack <shivasudharsank2008@gmail.com>
2024-10-09 20:21:21 +0530

    Update FlaskStyledTicketCode.py

--- FlaskStyledTicketCode.py
+++ FlaskStyledTicketCode.py
@@ -5,7 +5,7 @@ TicketCode = Flask(__name__)
 #first home page url
 @TicketCode.route("/home", methods = ["GET","POST"])
 def hello():
-    if request.method = "POST":
+    if request.method == "POST":
         username = request.form.get("username")
         return f"Your User Name: {username}"
     return render_template("Form.html")
