38e70ff10b64f489f647f14c8f893a40429bb511
Sys-stack <shivasudharsank2008@gmail.com>
2024-10-09 20:40:15 +0530

    Update FlaskStyledTicketCode.py

--- FlaskStyledTicketCode.py
+++ FlaskStyledTicketCode.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+import requests
+
+form = BytesIO(requests.get(https://raw.githubusercontent.com/Sys-stack/Ticket-Site/refs/heads/main/Form.html).content)
 TicketCode = Flask(__name__)
 
 
@@ -8,7 +11,7 @@ def hello():
     if request.method == "POST":
         username = request.form.get("username")
         return f"Your User Name: {username}"
-    return render_template("Form.html")
+    return render_template(form)
     
 if __name__ == '__main__':
     TicketCode.run(debug=True)
